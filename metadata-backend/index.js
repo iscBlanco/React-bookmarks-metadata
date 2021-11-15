0db79a3ce3a3060a764850b2b3738e98581a24af
Jose Luis Blanco Aguirre <iscblanco@gmail.com>
2021-11-15 01:14:46 -0600

    add cors library to nodejs project(metadata-backend)

--- metadata-backend/index.js
+++ metadata-backend/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { getHtml, getHtmlMetadata } from "./scrape";
 const app = express();
-const port = 3000;
+const port = 3002;
 
+app.use(cors());
 app.use(bodyParser.text());
 
 app.post("/", (req, res) => {
-  let url = req.body;
-  go(url).then((r) => res.send(r));
+  const url = res.json({ requestBody: req.body });
+  /* console.log(`tipo de url ${typeof url}, valor de url ${url}`);
+  go(url).then((r) => res.send(r)); */
+  console.log(url);
 });
 
 app.listen(port, () => {
