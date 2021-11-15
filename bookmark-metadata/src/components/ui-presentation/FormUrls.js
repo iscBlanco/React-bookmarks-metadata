f185b50c32441350dba066685aeec326af4fa530
Jose Luis Blanco Aguirre <iscblanco@gmail.com>
2021-11-15 02:18:49 -0600

    does scraping straight from REACT

--- bookmark-metadata/src/components/ui-presentation/FormUrls.js
+++ bookmark-metadata/src/components/ui-presentation/FormUrls.js
@@ -1,29 +1,58 @@
 import React, { useState } from "react";
 import axios from "axios";
+import cheerio from "cheerio";
 
 function FormUrls() {
   const [url, setUrl] = useState("");
   const [bookmarkList, setBookmarkList] = useState([]);
+  /* 
+  const addNewBookmark = (url) => {
+    axios.get(url).then(
+      (response) => {
+        if (response.status === 200) {
+          const html = response.data;
+          const $ = cheerio.load(html);
 
-  const addNewBookmark = async (url) => {
-    console.log(typeof url);
-    try {
-      let response = await axios.post("http://localhost:3002/", {
-        url,
-      });
-      console.log(response.data);
-      await response.data.then((r) => setBookmarkList(r));
+          const title = $("title").text();
+          const favicon = $('link[rel="shortcut icon"]').attr("href");
+          const image = $("img").attr("src");
+          setBookmarkList({
+            title: title,
+            favicon: favicon,
+            image: image,
+          });
+          console.log(`Este es el json agregado : ${bookmarkList[0]}`);
+        }
+      },
+      (e) => console.log(e)
+    );
+  }; */
 
-      console.log(bookmarkList);
-    } catch (e) {
-      console.error(e);
-    }
+  async function getHtml(url) {
+    const { data: html } = await axios.get(url);
+    return html;
+  }
+
+  let getHtmlMetadata = async (html) => {
+    const $ = await cheerio.load(html);
+    const title = $("title").text();
+    const favicon = $('link[rel="shortcut icon"]').attr("href");
+    const image = $("img").attr("src");
+    return { title: title, favicon: favicon, image: image };
   };
+
   const sendUrl = (event) => {
     event.preventDefault();
     console.log("enviando datos...", url);
-    setBookmarkList(addNewBookmark(url));
+
+    go(url).then((r) => setBookmarkList(r));
+    console.log(bookmarkList);
   };
+
+  const go = async (url) => {
+    return getHtmlMetadata(await getHtml(url));
+  };
+
   return (
     <div className="md">
       <form className="row" onSubmit={sendUrl}>
